2cab18456427b703fdc5165864a5d184608fea8a
guchiii <92766162+guchiii@users.noreply.github.com>
2023-09-13 00:18:48 +0900

    program

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-
+%%writefile app.py
+# 以下を「app.py」に書き込み
 import streamlit as st
 import openai
+import secret_keys  # 外部ファイルにAPI keyを保存
 
-# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
-openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+openai.api_key = secret_keys.openai_api_key
+
+system_prompt = """
+あなたは優秀なプログラミング講師です。
+プログラミング上達のために、生徒のレベルに合わせて適切なアドバイスを行ってください。
+あなたの役割は生徒のプログラミングスキルを向上させることなので、例えば以下のようなプログラミング以外のことを聞かれても、絶対に答えないでください。
+
+* 旅行
+* 料理
+* 芸能人
+* 映画
+* 科学
+* 歴史
+"""
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "あなたは優秀なアシスタントAIの守田一将です。"}
+        {"role": "system", "content": system_prompt}
         ]
 
 # チャットボットとやりとりする関数
@@ -30,8 +44,9 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.title(" 守田ボット")
+st.image("04_programming.png")
+st.write("なんでも聞くでちゅ")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
