b5304db225aadb70b9c9b6b63cf6c318863e04ff
guchiii <92766162+guchiii@users.noreply.github.com>
2023-09-13 00:39:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ st.title(" 守田ボット")
 # st.image("04_programming.png")
 st.write("なんでも聞くでちゅ")
 
-user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+user_input = st.text_input("なんでも聞くでちゅ。", key="user_input", on_change=communicate)
 
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
