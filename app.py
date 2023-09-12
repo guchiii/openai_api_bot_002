5b85450678b00c8b1238c7d84404c21abcb14163
guchiii <92766162+guchiii@users.noreply.github.com>
2023-09-13 00:14:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "
-        あなたは優秀なアシスタントAIの守田一将です。
-        "}
+        {"role": "system", "content": "あなたは優秀なアシスタントAIの守田一将です。"}
         ]
 
 # チャットボットとやりとりする関数
